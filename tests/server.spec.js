f06992fc5f60feef54a0f3312b3003c19c64c049
pblo <pdiazarenas541@gmail.com>
2026-02-24 10:15:40 +0000

    first commit

---
+++ tests/server.spec.js
@@ -0,0 +1,21 @@
+const request = require("supertest");
+const server = require("../index");
+
+describe("Operaciones CRUD de cafes", () => {
+
+    it("GET/cafes satus code = 200 & tipo de dato = Array & Array.length >= 1", ()=>{
+
+    })
+
+    it("DELETE/cafes/:id devuelve status code 404 si id no existe", ()=>{
+
+    })
+
+    it("POST/cafes agregar producto y devuelve status code 201", ()=>{
+
+    })
+
+    it("PUT/cafes devuelve status code 400 con id de parametros != id de payload",()=>{
+        
+    })
+});
